4a7ef73b6b521c5a0e0bace41de2861b99a73337
tuyenhn <hntuyen282@gmail.com>
2023-08-16 11:18:34 +0700

    toy data and function

---
+++ R/spagr.R
@@ -0,0 +1,30 @@
+library(stars)
+library(sf)
+
+chirps_rast <- read_stars("chirps.tif")
+vnm_shp <- read_sf("gadm41_VNM_2.shp")
+
+spagr <- function(rast, shp, method = 1, ...) {
+  if (class(rast)[1] != "stars") {
+    print("`rast` has to be a `stars` object")
+    return
+  }
+  if (class(shp)[1] != "sf") {
+    print("`shp` has to be an `sf` object")
+    return
+  }
+  if (class(method) != "numeric" && class(method) != "function") {
+    print("`method` has to be a number between 1-3 or a defined function")
+    return
+  }
+  if (!(method %in% c(1, 2, 3))) {
+    print("`method` has to be a number between 1-3")
+    return
+  }
+
+  spagr_(rast = rast, shp = shp, method = method)
+}
+
+spagr_ <- function(rast, shp, method) {
+  method <- methods[method]
+}
